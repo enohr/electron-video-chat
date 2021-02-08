840e44a3a9751e4665c6de6879c67e8ad446bf5f
Eduardo Lessa <duduuulessa@gmail.com>
2021-02-07 23:29:52 -0300

    Feat: get screen share from webrtc

--- src/utils/media.js
+++ src/utils/media.js
@@ -2,4 +2,8 @@ class Media {
     async getDevices(video = true, audio = true) {
         return navigator.mediaDevices.getUserMedia({video, audio});
     }
+
+    async getScreenShare(displayMediaOptions) {
+        return navigator.mediaDevices.getDisplayMedia(displayMediaOptions);
+    }
 }
